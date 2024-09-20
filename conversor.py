e7ed1704fda973a47ff36c239ffbeacc135aa543
JuanCarlos <meloza@LAPTOP-VL17EOVM.localdomain>
2024-09-20 16:51:00 +0200

    Primera version, se introduce la url por input

--- conversor.py
+++ conversor.py
@@ -4,4 +4,17 @@ Este programa extraera el texto de una url dada para luego convertir ese texto a
 import nltk
 from newspaper import Article
 from gtts import gTTS
+#import nltk.downloader
+#descargar recursos necesarios para nltk
+nltk.download('punkt')
+
+#creamos una variable tipo string donde almazenamos la url que vamos a utilizar.
+url=str(input("Introduce la url del texto que vamos a pasar a mp3:"))
+article=Article(url)
+article.download()
+article.parse()
+texto=article.text
+tts=gTTS(texto, lang='es')
+tts.save("noticia.mp3")
+
 
