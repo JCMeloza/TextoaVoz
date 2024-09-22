8cc2b43bf30357698b49d419496e8eb18749a57d
JuanCarlos <meloza@LAPTOP-VL17EOVM.localdomain>
2024-09-22 16:22:27 +0200

    añadiendo la utilizacion de nltk para tokenizar el texto con sent_tokenize

--- conversor.py
+++ conversor.py
@@ -4,17 +4,30 @@ Este programa extraera el texto de una url dada para luego convertir ese texto a
 import nltk
 from newspaper import Article
 from gtts import gTTS
-#import nltk.downloader
+
 #descargar recursos necesarios para nltk
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 #creamos una variable tipo string donde almazenamos la url que vamos a utilizar.
 url=str(input("Introduce la url del texto que vamos a pasar a mp3:"))
+
+# Extraer y procesar el artículo con newspaper3k y nltk
 article=Article(url)
 article.download()
 article.parse()
-texto=article.text
+
+#dividimos el texto en oraciones con nltk
+oraciones=nltk.sent_tokenize(article.text)
+text=article.text
+
+#unimos las oraciones en un texto limpio
+texto= " ".join(oraciones)
+
+#convertimos el texto en un archivo mp3 con gtts
 tts=gTTS(texto, lang='es')
-tts.save("noticia.mp3")
+tts.save("noticia_n.mp3")
+
+print("El artículo ha sido convertido a voz y guardado.")
 
 
