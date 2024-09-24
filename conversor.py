dd1cb2f71569e94c9b1670d9b3ca14487dfb2b8a
JuanCarlos <meloza@LAPTOP-VL17EOVM>
2024-09-24 13:10:25 +0200

    Añadiendo parte grafica con streamlit

--- conversor.py
+++ conversor.py
@@ -4,30 +4,87 @@ Este programa extraera el texto de una url dada para luego convertir ese texto a
 import nltk
 from newspaper import Article
 from gtts import gTTS
+import streamlit as st
+import time
 
-#descargar recursos necesarios para nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
 
-#creamos una variable tipo string donde almazenamos la url que vamos a utilizar.
-url=str(input("Introduce la url del texto que vamos a pasar a mp3:"))
+#funcion para extraer el texto de una url, devuelve el texto extraido
+def extraer_texto_url(url):
+    
+    #descargar recursos necesarios para nltk
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    
+    # Extraer y procesar el artículo con newspaper3k y nltk
+    article=Article(url)
+    article.download()
+    article.parse()
+    noticia= article.text
+    
+    #dividimos el texto en oraciones con nltk
+    oraciones=nltk.sent_tokenize(article.text)
+    text=article.text
 
-# Extraer y procesar el artículo con newspaper3k y nltk
-article=Article(url)
-article.download()
-article.parse()
+    #unimos las oraciones en un texto limpio
+    texto= " ".join(oraciones)
+    return texto
 
-#dividimos el texto en oraciones con nltk
-oraciones=nltk.sent_tokenize(article.text)
-text=article.text
+#funcion para convertir el texto en audio y guardarlo en un archivo mp3
+def convertir_audio(texto,idioma):
+    
+    #convertimos el texto en un archivo mp3 con gtts
+    tts=gTTS(texto, lang=idioma)
+    st.info("Guardando el archivo mp3 .....espere")
+    #guardar el archivo mp3
+    tts.save("noticia.mp3")
+    
+    #mostramos mensaje de éxito
+    st.success("¡Conversión completada!")
 
-#unimos las oraciones en un texto limpio
-texto= " ".join(oraciones)
 
-#convertimos el texto en un archivo mp3 con gtts
-tts=gTTS(texto, lang='es')
-tts.save("noticia_n.mp3")
+#definimos las diferentes paginas para navegar
+def page_1():
+   
+    st.subheader("🗞️ Extraer audio de una noticia")
 
-print("El artículo ha sido convertido a voz y guardado.")
+    
+
+    with st.form("Introduce la url"):
+        url = st.text_input("URL", value="", key="url_text")
+        submit_url=st.form_submit_button("Enviar")
+        
+        if submit_url:
+            
+            texto= extraer_texto_url(url)
+            convertir_audio(texto,"es")
+
+def page_2():
+    st.subheader("✍️ Escribe un texto para obtener su audio")
+
+    with st.form("Introduce un texto"):
+        texto = st.text_area("Introduce un texto", value="", key="texto_text")
+        submit_texto=st.form_submit_button("Enviar")
+        
+        if submit_texto:
+            
+            #texto= extraer_texto_url(url)
+            convertir_audio(texto,"es")
+
+
+#creamos las categorias en el Sidebar
+pg= st.navigation(
+    {
+        "Noticia": [
+            st.Page(page_1,title="Obtener audio de noticia ", icon="📰"),
+        ],
+        "Texto": [
+            st.Page(page_2, title="Obtener audio de texto", icon="✒️")
+        ]
+    }
+)
+#iniciamos la paginacion
+pg.run()
+
+
 
 
